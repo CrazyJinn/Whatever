47cd0ed961790b20a6017c5db130bd63b53a8bed
CrazyJinn <CrazyJinn@qq.com>
2013-03-08 10:21:27 +0800

    User和Tag的Model做好了，为Service添加了一个单元测试

--- Model/User.cs
+++ Model/User.cs
@@ -12,8 +12,47 @@ namespace Model
 
         public string Password { get; set; }
 
-        public int Money { get; set; } //改名
+        public int Money { get; set; }
 
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public Gender Gender { get; set; }
+
+        /// <summary>
+        /// Mac网卡地址
+        /// </summary>
+        public string Mac { get; set; }
+
+        /// <summary>
+        /// 注册时间
+        /// </summary>
         public DateTime RegisterTime { get; set; }
+
+        public UserType UserType { get; set; }
+
+        /// <summary>
+        /// 用户状态,比如说被锁之类的
+        /// </summary>
+        public UserStatus UserStatus { get; set; }
+
+        public List<Tag> Tags { get; set; }
+    }
+
+    public enum Gender
+    {
+        Man = 1,
+        Women = 2,
+    }
+
+    public enum UserStatus
+    {
+        Active = 1,
+        InActive = 2,
+        Lock = 4,
+    }
+
+    public enum UserType
+    {
     }
 }
