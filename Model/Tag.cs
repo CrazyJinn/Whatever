cc76795ade95be17efaeb5f52e29d79493a0e319
CrazyJinn <CrazyJinn@qq.com>
2013-03-08 10:31:43 +0800

    Tag没上传成功？

---
+++ Model/Tag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 类别标签
+    /// </summary>
+    public class Tag : Identifier
+    {
+        public string TagName { get; set; }
+
+        /// <summary>
+        /// 购买此Tag需要的钱
+        /// </summary>
+        public int NeedMoney { get; set; }
+    }
+}
