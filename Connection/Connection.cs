5b576e3a38bc6af846ea1a689f776c628d961462
crazyjinn <crazyjinn@qq.com>
2014-02-20 17:33:18 +0800

    简化了一下

--- Connection/Connection.cs
+++ Connection/Connection.cs
@@ -4,14 +4,8 @@ namespace Connection
 {
     public class Connection
     {
-        public static MongoServer GetServer(string connString) {
-            //string connectionString = "mongodb://localhost";
-            string connectionString = connString;
-            return MongoServer.Create(connectionString);
-        }
-
         public static MongoCollection GetCollection(string connString, string collectionName) {
-            MongoServer server = GetServer(connString);
+            MongoServer server = MongoServer.Create(connString);
             string DBname = "Whatever_" + collectionName;       //使用"Whatever_+ collectionName作为DBname
             MongoDatabase db = server.GetDatabase(DBname);
             return db.GetCollection(collectionName);
